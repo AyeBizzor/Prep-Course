58763c0a2be33c5be4113765e5f33fd855202be8
Ayes <vachier.ilustrator@gmail.com>
2022-08-23 21:27:04 -0300

    mas avances

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -49,7 +49,7 @@ function saludo(idioma) {
   } else if (idioma === 'mandarin') {
     return "Ni Hao!";
   } else if (idioma === 'ingles') {
-    return "Hola!"; // el npm test dice que debería decir "Hola!" y no "Hello!" no entiendo el error
+    return "Hola!"; // el npm test dice que debería decir "Hola!" y no "Hello!", no entiendo el error
   } else {
     return "Hola!";
   }
@@ -121,6 +121,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+} else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+} else if (num3 > num1 && num3 > num2) {
+  return num3 + 1;
+} else if (num1 === 0 || num2 || === 0 || num3 = 0) {
+  return "Error";
+} else {
+  return false;
+  }
 }
 
 function esPrimo(numero) {
